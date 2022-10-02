6337ad065b0d1a9a2316ad992b917aaff73125ff
Melika Hamedani <97775710+Melikahamedani@users.noreply.github.com>
2022-10-02 17:37:58 -0400

    Update server.js

--- server.js
+++ server.js
@@ -10,71 +10,57 @@
  * Your app’s URL (from Cyclic) :
  * 
  * *************************************************************************/
- var HTTP_PORT = process.env.PORT || 8080;  
- var express = require('express'); 
- var app = express();
- var path = require('path');
- var dataService = require('./data-service')
+let HTTP_PORT = process.env.PORT || 8080;
+let express = require("express");
+let app = express();
+let path = require("path");
+let dataService = require("./data-service");
 
-
- function onHttpStart() {
+function onHttpStart() {
     console.log("Express http server listening on: " + HTTP_PORT);
 }
 
+app.use(express.static('./public/site.css'));
 
-app.use(express.static('public'))
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname,"/views/home.html"));
+});
 
+app.get("/about", function(req,res){
+    res.sendFile(path.join(__dirname,"/views/about.html"));
+});
 
-//setup route to listen on /
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/home.html'));
+app.get("/departments", function(req,res){
+    dataService.getDepartments().then((data) =>{
+        res.json(data);
+    }).catch((err)=>{
+        res.json({ error: err });
+    })
+});
+
+app.get("/employees", function(req,res){
+    console.log("---------");
+    dataService.getAllEmployees().then((data) => {
+        res.json(data);
+    }).catch((err) =>{
+        res.json({ error: err })
+    })
+});
+
+app.get("/managers", function(req,res){
+    dataService.getManagers().then((data) =>{
+        res.json(data);
+    }).catch((err) =>{
+        res.json({ error: err })
+    })
+});
+
+app.get('*', function (req, res) {
+    res.sendFile('/public/image/404.jpg', { root: __dirname })
 })
 
 
-// setup another route to listen on /about
-app.get('/about', function(req, res){
-    res.sendFile(path.join(__dirname, '/views/about.html'));
-});
-
- //Departments
- app.get('/departments', function(req,res){
-    dataService.getDepartments().then((data) => {
-        res.json(data);
-    }).catch((err)=>{
-        res.json({message: err});
-    })
-});
-
-
-//Employees
-app.get('/employees', function(req,res){
-    dataService.getAllEmployees().then((data) => {
-        res.json(data);
-    }).catch((err)=>{
-        res.json({message:err});
-    })
-});
-
-
-//Managers
-app.get('/managers', function(req,res){
-    dataService.getManagers().then((data) => {
-        res.json(data);
-        console.log("TODO: get all employees who have isManager==true");
-    }).catch((err)=>{
-        res.json({message:err});
-    })
-});
-
-
- //get error 404
-app.get("/*", function(req,res){
-    res.status(404).json({ error: 'Page Not Found' })
-});
-
-
-//initialize
-data_service.initialize().then(() => {
+dataService.initialize().then(() => {
     app.listen(HTTP_PORT, onHttpStart);
 }).catch(() => {
     console.log("Unable to load data");
