a00a8e575bd602dca66cfbdea1fd80f26abb380a
MelikaHamedani <mhamedani@myseneca.ca>
2022-10-02 13:51:34 -0400

    initial commit

--- data-service.js
+++ data-service.js
@@ -4,26 +4,24 @@ let departments = [];
 
 
 //initialize()
-module.exports.initialize =function () {
-    return new Promise ((resolve, reject) => {
-        try{
-            fs.readFile("./data/employees.json", (err, data) => {
-                if (err) reject("Unable to read the file");
-                resolve();
-              });
-              fs.readFile("./data/programs.JSON", (err, data) => {
-                programs = JSON.parse(data);
-              });
-        }catch(err){
-                reject("unable to read file")
-            }
-        resolve("success");
-    })
-};
+module.exports.initialize = function () {
+    return new Promise((resolve, reject) => {
+      fs.readFile("./data/employees.json", (err, data) => {
+        if (err) reject("Failure to read file employees.json!");
+        employees = JSON.parse(data);
+        resolve();
+      });
+      fs.readFile("./data/departments.json", (err, data) => {
+        if (err) reject("Failure to read file departments.json!");
+        programs = JSON.parse(data);
+      });
+      resolve();
+    });
+  };
 
 
 //getAllEmployees
-exports.getAllEmployees = function() {
+module.exports.getAllEmployees = function() {
     return new Promise((resolve, reject) => {
         if (employees.length == 0){
             reject(Error("no results returned"))
@@ -36,7 +34,7 @@ exports.getAllEmployees = function() {
 
 
 //getManagers
-exports.getManagers = function() {
+module.exports.getManagers = function() {
     return new Promise((resolve, reject) => {
         const managers = []
         for (let i = 0; i < employees.length; i++) {
